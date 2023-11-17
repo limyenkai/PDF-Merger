85669a73b9aed49618133c540fc5bac68b403602
Lim Yen Kai <yenkai.lim@ctc-g.com.sg>
2023-11-17 11:02:28 +0800

    first commit

--- pdfmerger.py
+++ pdfmerger.py
@@ -5,10 +5,13 @@ import os
 merger = PdfWriter()
 current_dir = os.curdir
 combined_dir = os.path.join(current_dir, 'Input Files')
+output_path = os.path.join(current_dir, 'Output File', 'CombinedPDF.pdf')    
 
 print(combined_dir)
 
 for file in os.listdir(combined_dir): 
     if file.endswith(".pdf"):
-        merger.append(file)
-    merger.write("Output File/CombinedPDF.pdf")
+        file_path = os.path.join(combined_dir, file)
+        merger.append(file_path)
+
+merger.write(output_path)
